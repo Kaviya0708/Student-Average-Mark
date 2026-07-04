0affbb07510aa515b81daf3cdf617711fdc7f0c3
Kaviya <732125104051.25.cse@nandhatech.org>
2026-07-04 08:16:46 +0530

    Create Main6.java

---
+++ Main6.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int mark1 = 78;
+        int mark2 = 84;
+        int mark3 = 88;
+
+        double average = (mark1 + mark2 + mark3) / 3.0;
+
+        System.out.println("Mark 1 = " + mark1);
+        System.out.println("Mark 2 = " + mark2);
+        System.out.println("Mark 3 = " + mark3);
+        System.out.println("Average Mark = " + average);
+    }
+}
